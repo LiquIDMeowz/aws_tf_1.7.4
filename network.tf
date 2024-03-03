a8c6898dbec93e70bc1c7d8ffd1cf8dc1f8a4909
LiquIDMeowz <v_vkrastev@abv.bg>
2024-03-03 20:32:39 +0200

    tf fmt

--- network.tf
+++ network.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 module "network" {
-  source              = "./modules/network"
-  vpc_cidr            = var.vpc_cidr
-  subnet_cidr         = var.subnet_cidr
-  availability_zones  = var.availability_zones
+  source             = "./modules/network"
+  vpc_cidr           = var.vpc_cidr
+  subnet_cidr        = var.subnet_cidr
+  availability_zones = var.availability_zones
 }
--- variables.tf
+++ variables.tf
@@ -5,10 +5,16 @@ variable "vpc_cidr" {
 
 variable "subnet_cidr" {
   description = "The CIDR blocks for the subnets"
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"]
 }
 
 variable "availability_zones" {
   description = "The availability zones for the public subnets"
-  default = ["eu-central-1a", "eu-central-1b", "eu-central-1c" ]
+  default = [
+    "eu-central-1a",
+    "eu-central-1b",
+    "eu-central-1c"]
 }
